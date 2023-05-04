0bd3d8eeff764f959f4a6a6bcd990b6929334d38
Muhammad Gumilang Tamasakti <gumilangtamasakti@gmail.com>
2023-05-04 13:16:16 +0700

    Folder structuring for projects

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main>
+      <h1>FIlmKu App</h1>
+    </main>
+  );
+}
